a7ae4ee672b01828d0810939f59e95195472a28c
Lynn <lynnz.dev@gmail.com>
2024-05-07 19:58:21 -0400

    mock book boject

--- TCASimple/TCASimple/Book.swift
+++ TCASimple/TCASimple/Book.swift
@@ -10,3 +10,19 @@ struct Book: Codable, Equatable {
     let mediaType, released: String
     let characters, povCharacters: [String]
 }
+
+extension Book {
+    static let mock: Book = Book(url: "https://anapioficeandfire.com/api/books/1",
+                                 name: "A Game of Thrones",
+                                 isbn: "978-0553103540",
+                                 authors: ["George R. R. Martin"],
+                                 numberOfPages: 694,
+                                 publisher: "Bantam Books",
+                                 country: "United States",
+                                 mediaType: "Hardcover",
+                                 released: "1996-08-01T00:00:00",
+                                 characters: ["https://anapioficeandfire.com/api/characters/2",
+                                              "https://anapioficeandfire.com/api/characters/12"],
+                                 povCharacters: ["https://anapioficeandfire.com/api/characters/148",
+                                                 "https://anapioficeandfire.com/api/characters/208"])
+}
