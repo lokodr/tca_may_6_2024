045225dd2c92a949d7514be3bec95eada29ab6c5
Lynn <lynnz.dev@gmail.com>
2024-05-07 21:02:45 -0400

    add stackstate and stackaction to booklistreducer

--- TCASimple/TCASimple/BookListReducer.swift
+++ TCASimple/TCASimple/BookListReducer.swift
@@ -3,12 +3,22 @@ import Foundation
 
 class BookListReducer: Reducer {
     struct State: Equatable {
+        /*
+         In state, we create a StackState named path. This variable will hold the states of the detail pages we will open..
+         */
+        /// A list of data representing the content of a navigation stack.
+        var path = StackState<BookDetailReducer.State>()
         var books: [Book]?
     }
 
     enum Action {
         case fetchBooks
         case booksFetched([Book]?)
+        /*
+         Then, we add a path action in the Action enum, which takes the State and Action enum of the detail page as parameters.
+         */
+        /// A wrapper type for actions that can be presented in a navigation stack.
+        case path(StackAction<BookDetailReducer.State, BookDetailReducer.Action>)
     }
 
     func reduce(into state: inout State, action: Action) -> Effect<Action> {
@@ -22,6 +32,8 @@ class BookListReducer: Reducer {
             }
         case let .booksFetched(books):
             state.books = books
+            return .none
+        case .path:
             return .none
         }
     }
